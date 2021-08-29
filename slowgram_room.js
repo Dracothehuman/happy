1bc5b8a9c2ae440d26ec00300641f9fa70141d16
Dracothehuman <80511020+Dracothehuman@users.noreply.github.com>
2021-08-29 12:55:44 +1000

    Add files via upload

---
+++ slowgram_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAl6qMS9w5JwSqCUqgTdgcohkOD2iSpnNI",
+    authDomain: "slowgram-f53a8.firebaseapp.com",
+    databaseURL: "https://slowgram-f53a8-default-rtdb.firebaseio.com",
+    projectId: "slowgram-f53a8",
+    storageBucket: "slowgram-f53a8.appspot.com",
+    messagingSenderId: "1063914086518",
+    appId: "1:1063914086518:web:b075dfa125463810b13f57"
+  };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
